4e9c1993b6e0f1bdcc9dd40e70ed56cae31ec63b
Julian Alonso <tantissimo.entusiasmo@gmail.com>
2024-08-20 17:20:00 +0200

    Model added, i18n added, removed init

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,7 +1,9 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent"
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/BindingMode"
 ], (UIComponent) => {
 	"use strict";
+
 	return UIComponent.extend("ui5.databinding.Component", {
 		metadata: {
 			interfaces: ["sap.ui.core.IAsyncContentCreation"],
